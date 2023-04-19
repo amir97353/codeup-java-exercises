4741c5edc189d73bce0756531733bd87c54edab9
Amir Saunders <amir97353@gmail.com>
2023-04-19 18:26:58 -0500

    Hello World

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,36 @@
 public class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello, \n World!");
+        System.out.println("Hello world");
+        int myFavoriteNumber = 11;
+        String myString = "My last day";
+        float myNumber = 3.14f;
+        System.out.println(myFavoriteNumber);
+        System.out.println(myString);
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        int x = 4;
+//        x += 5;
+//
+//        int x = 3;
+//        int y = 4;
+//
+//        y *= x;
+//
+//
+//        int x = 10;
+//        int y = 2;
+//
+//        x /= y;
+//        y -= x;
+
     }
 }
