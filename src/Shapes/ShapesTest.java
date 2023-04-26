1c8b40e80cb9c10dfc893231174a4fc7cb4249cf
Amir Saunders <amir97353@gmail.com>
2023-04-25 20:11:28 -0500

    assignmnet update

--- src/Shapes/ShapesTest.java
+++ src/Shapes/ShapesTest.java
@@ -1,14 +1,17 @@
-//package Shapes;
-//
-//public class ShapesTest {
-//    public static void main(String[] args) {
-//        Rectangle box1 = new Rectangle(4,5);
-//        System.out.println(box1.getPeremier());
-//        System.out.println(box1.getArea());
-//
-//        Rectangle box2 = new Square(5);
-//        System.out.println(box2.getPeremier());
-//        System.out.println();
-//
-//    }
-//}
+
+
+
+package Shapes;
+
+public class ShapesTest {
+    public static void main(String[] args) {
+        Rectangle box1 = new Rectangle(4, 5);
+        System.out.println(box1.getPerimeter());
+        System.out.println(box1.getArea());
+
+        Rectangle box2 = new Square(5);
+        System.out.println(box2.getPerimeter());
+        System.out.println(box2.getArea());
+
+    }
+}
--- src/Shapes/Square.java
+++ src/Shapes/Square.java
@@ -1,11 +1,21 @@
-//package Shapes;
-//
-//public class Square extends Rectangle {
-//    public void Square(double side){
-//        super(side,side);
-//    }
-//
-//    public Square(double length, double width) {
-//        super(length, width);
-//    }
-//}
+package Shapes;
+
+public class Square extends Rectangle{
+    public double side;
+
+    public Square(double side){
+        super(side, side);
+        this.side = side;
+    }
+
+    public double getPerimeter(){
+        //double side = this.width;
+        return side*4;
+    }
+
+    public double getArea(){
+        //double side = this.width;
+        return Math.pow(side, 2);
+    }
+
+}
