114dc903fe1816a13efcdad72fd4cfe1748403a1
Amir Saunders <amir97353@gmail.com>
2023-05-01 09:43:13 -0500

    update

--- src/Survey.java
+++ src/Survey.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Survey {
     public static void main(String[] args) {
 
-        //*********PART A: PICKING UP THE USER'S ANSWERS*********
+
         System.out.println("Welcome. Thank you for taking the survey");
 
         Scanner scanner = new Scanner(System.in);
@@ -22,7 +22,7 @@ public class Survey {
         System.out.println("How many times a week do you buy fast food?");
         int foodAmount = scanner.nextInt();
 
-        //*********PART B: RESPONDING TO THE USER**********
+
         System.out.println("Thank you <name> for answering all <counter> questions");
         System.out.println("Weekly, you spend $<totalPrice> on coffee");
         System.out.println("Weekly, you spend $<totalPrice> on food");
